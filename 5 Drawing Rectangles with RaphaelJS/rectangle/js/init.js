e24c1b433266d45fcd06604844a75b8edb36a978
Truston Ailende <trustonailende@gmail.com>
2015-03-12 06:05:59 +0100

    Addition of code files for Week 5

---
+++ 5 Drawing Rectangles with RaphaelJS/rectangle/js/init.js
@@ -0,0 +1,6 @@
+$(function(){
+	var paper = Raphael('container', 500, 500);
+	
+	// Draw a red rectangle
+	var rectangle = paper.rect(100, 100, 200, 100).attr("fill", "red");
+});
---
+++ 5 Drawing Rectangles with RaphaelJS/rectangle_rounded/js/init.js
@@ -0,0 +1,6 @@
+$(function(){
+	var paper = Raphael('container', 500, 500);
+	
+	// Draw a rectangle with rounded corners
+	var rectangle = paper.rect(100, 100, 200, 100, 40).attr("fill", "red");
+});
